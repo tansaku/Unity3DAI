0302f207d457984052a115364178a81b1acb72a7
Sam Joseph <tansaku@gmail.com>
2012-11-21 10:38:53 +0000

    Kinematic Arrive working

--- Assets/AI-Scripts/Seek.js
+++ Assets/AI-Scripts/Seek.js
@@ -5,17 +5,30 @@ function Start () {
 }
 
 var maxSpeed = 10.0;
+var radius = 0.01;
 var target:Transform;
 var velocityVector:Vector3;
 var direction:Vector3;
 var steeringVector:Vector3;
 
 function Update () {
+	Arrive();
+	transform.Translate(steeringVector * Time.deltaTime);
+}
 
+function Arrive() {
+	velocityVector = target.position - this.transform.position;
+	if(velocityVector.magnitude > maxSpeed){
+	  velocityVector.Normalize();
+	  steeringVector = velocityVector *  maxSpeed;
+	} 
+    if(velocityVector.magnitude < radius)
+      steeringVector = Vector3(0,0,0);
+}
+
+
+function Seek() {
 	velocityVector = target.position - this.transform.position;
 	velocityVector.Normalize();
 	steeringVector = velocityVector *  maxSpeed; 
-	
-	transform.Translate(steeringVector * Time.deltaTime);
-	
 }
